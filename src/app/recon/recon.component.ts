b90cae991fb46eb3b1b50b376e2fd4a80f37d8ab
BigGlaucos <glaucoscc@gmail.com>
2023-11-30 01:08:04 -0300

    Face Recon add

--- src/app/recon/recon.component.ts
+++ src/app/recon/recon.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
+
+
+declare const faceapi: any; 
+
 @Component({
-  selector: 'app-recon',
+  selector: 'recon',
   templateUrl: './recon.component.html',
   styleUrls: ['./recon.component.css']
 })
-export class ReconComponent implements OnInit{
-      constructor(public router : Router){}
-  ngOnInit(): void {
-    
+export class FaceDetectionComponent {
+  @ViewChild('imageInput')
+  imageInput!: ElementRef;
 
+  constructor(private router: Router) {}
 
-  }
+  async detectFaces(): Promise<void> {
+    const input = this.imageInput.nativeElement as HTMLInputElement;
 
-
-  
+    if (input.files && input.files.length > 0) {
+      const imgFile = input.files[0];
+      const img = await faceapi.bufferToImage(imgFile);
       
+      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+      
+      const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions());
+
+      if (detections && detections.length > 0) {
+        this.router.navigate(['\acess']); 
+      } else {
+        console.log('No faces detected.');
+      }
+    }
+  }
 }
